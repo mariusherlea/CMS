9b1d6322102149180f42ea93f445df17cef82a14
Marius Herlea <mariusherlea@gmail.com>
2019-02-27 10:00:45 +0200

    cms

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<?php include "includes/admin-header.php"; ?>
+
+    <div id="wrapper">
+
+
+
+        <!-- Navigation -->
+
+
+        <?php include "includes/admin-nav.php"; ?>
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+
+                           <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+
+
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+
+
+    <?php include "includes/admin-footer.php"?>
