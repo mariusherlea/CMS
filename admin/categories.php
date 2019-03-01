c8f092985ae564c4bd9a8496a4c69a7718110beb
Marius Herlea <mariusherlea@gmail.com>
2019-03-01 12:36:13 +0200

    update&refactoring

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,5 @@
 <?php include "includes/admin-header.php"; ?>
+    <?php include "function.php"; ?>
 
     <div id="wrapper">
 
@@ -28,30 +29,7 @@
                         <div class="col-xs-6">
 
 
-                        <?php
-
-                            if(isset($_POST['submit'])){
-
-                                $cat_title = $_POST['cat_title'];
-
-                                if ($cat_title == "" || empty($cat_title)){
-
-                                    echo "This field shoul not be empty";
-
-                                } else {
-
-                                    $query = "INSERT INTO categories(cat_title)";
-                                    $query .= "VALUE ('{$cat_title}')";
-                                    $create_categories_query = mysqli_query($connection, $query);
-
-                                    if (!$create_categories_query){
-                                        die('query failed'.mysqli_error($connection));
-                                    }
-                                }
-
-                            }
-
-                            ?>
+                        <?php  insert_categories(); ?>
 
 
                         <form action="" method="post">
@@ -67,42 +45,17 @@
 
                         </form>
 
-                         <form action="" method="post">
-                           <div class="form-group">
-                              <label for="cat_title">Update Categories</label>
+                       <?php
 
-                              <?php
-                                if(isset($_GET['update'])){
-                                    $cat_id=$_GET['update'];
-                               $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
-                        $select_categories_id = mysqli_query($connection, $query);
+                            if(isset($_GET['update'])){
 
-                            while($row = mysqli_fetch_assoc($select_categories_id)){
+                                $cat_id = $_GET['update'];
 
-                                $cat_id= $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                ?>
-
-                                 <input value="<?php if(isset($cat_title)) {
-
-                                    echo ($cat_title);
-
-                                }?>" class="form-control" type="text" name="cat_title">
-
-                            <?php }}
-
-                               ?>
+                            include "includes/edit-categ.php";
 
 
-
-
-                           </div>
-                           <div class="form-group">
-                               <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
-
-                           </div>
-
-                        </form>
+                            }
+                            ?>
 
                         </div>
 
