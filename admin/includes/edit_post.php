dcf904ac7b3903d438cb01bd3f210ad79e55aa89
Marius Herlea <mariusherlea@gmail.com>
2019-03-04 14:08:03 +0200

    update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,12 +1,12 @@
 <?php
-
+global $connection;
 if(isset($_GET['p_id'])){
 
    $the_post_id = $_GET['p_id'];
 }
 
- global $connection;
-                $query = "SELECT * FROM posts";
+
+                $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
                 $select_posts_by_id = mysqli_query($connection, $query);
 
             while($row = mysqli_fetch_assoc($select_posts_by_id)){
@@ -24,6 +24,40 @@ if(isset($_GET['p_id'])){
 
             }
 
+if(isset($_POST['update_post'])){
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    //$post_status = $_POST['post_status'];
+    //$post_image = $_FILES['image']['name'];
+    //$post_image_temp = $_FILES['image']['name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+//move_uploaded_file($post_image_temp, "../image/$post_image");
+
+
+
+    $query = "UPDATE posts SET ";
+    $query .="post_title = '{$post_title}', ";
+    $query .="post_category_id = '{$post_category_id}', ";
+    $query .="post_date = now(), ";
+    $query .="post_author = '{$post_author}', ";
+    //$query .="post_status = '{$post_status}', ";
+    $query .="post_tags = '{$post_tags}', ";
+    $query .="post_content = '{$post_content}' ";
+   // $query .="post_image = '{$post_image}' ";
+    $query .=" WHERE post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+
+    comfirmQuery($update_post);
+
+}
+
+
+
 ?>
 
 
@@ -34,7 +68,7 @@ if(isset($_GET['p_id'])){
 
       <div class="form-group">
          <label for="title">Post Title</label>
-          <input type="text" value="<?php echo $post_title; ?>" class="form-control" name="title">
+          <input type="text" value="<?php echo $post_title; ?>" class="form-control" name="post_title">
       </div>
 
 
@@ -45,7 +79,7 @@ if(isset($_GET['p_id'])){
 
           <div class="form-group">
          <label for="title">Post Author</label>
-          <input type="text" value="<?php echo $post_author; ?>" class="form-control" name="author">
+          <input type="text" value="<?php echo $post_author; ?>" class="form-control" name="post_author">
       </div>
 
          <div class="form-group">
@@ -89,7 +123,7 @@ if(isset($_GET['p_id'])){
 
       <div class="form-group">
          <label for="post_content">Post Content</label>
-         <textarea class="form-control"  name="post_content" id="" cols="30" rows="10"> <?php echo $post_content; ?>
+         <textarea class="form-control"  name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?>
          </textarea>
       </div>
 
@@ -97,7 +131,7 @@ if(isset($_GET['p_id'])){
 
 
        <div class="form-group">
-          <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+          <input class="btn btn-primary" type="submit" name="update_post" value="Publish Post">
       </div>
 
 
