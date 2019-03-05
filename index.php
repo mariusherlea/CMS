5bca58b3b2c5bd981c7cf42eba392ba357cacfbf
Marius Herlea <mariusherlea@gmail.com>
2019-03-05 14:32:04 +0200

    individual page post

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
         $query = "SELECT * FROM posts";
         $select_all_posts_query = mysqli_query($connection,$query);
         while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
+                $post_id = $row['post_id'];
+            $post_title = $row['post_title'];                $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
                 $post_content = $row['post_content'];
@@ -39,7 +39,7 @@
 
 
                 <h2>
-                    <a href="#"><?php echo $post_title ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
