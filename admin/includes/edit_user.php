b0d7483b1c06fec289c943686ef7b521b674ccf9
Marius Herlea <mariusherlea@gmail.com>
2019-03-17 12:25:03 +0200

    update user

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,10 +1,25 @@
 <?php
 
-
 if(isset($_GET['edit_user'])){
+ $the_user_id =$_GET['edit_user'];
 
-   echo $the_user_id = $_GET['edit_user'];
-}
+      $query = "SELECT * FROM users WHERE user_id=$the_user_id ";
+            $select_users_query = mysqli_query($connection, $query);
+
+            while($row = mysqli_fetch_assoc($select_users_query)){
+
+                $user_id= $row['user_id'];
+               $username = $row['username'];
+                $user_password= $row['user_password'];
+                $user_firstname= $row['user_firstname'];
+                $user_lastname= $row['user_lastname'];
+                $user_email = $row['user_email'];
+                $user_role = $row['user_role'];
+
+}}
+
+
+
 
 
 if(isset($_POST['edit_user'])){
@@ -18,15 +33,19 @@ if(isset($_POST['edit_user'])){
         $user_password      = $_POST['user_password'];
 
 
- $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) ";
+        $query = "UPDATE users SET ";
+        $query .="user_firstname = '{$user_firstname}', ";
+        $query .="user_lastname = '{$user_lastname}', ";
+        $query .="user_role = '{$user_role}', ";
+        $query .="username = '{$username}', ";
+        $query .="user_email = '{$user_email}', ";
+        $query .="user_password = '{$user_password}' ";
 
-      $query .= "VALUES('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}','{$user_password}') ";
+        $query .=" WHERE user_id = {$the_user_id} ";
 
-      $create_post_query = mysqli_query($connection, $query);
+        $edit_user_query = mysqli_query($connection, $query);
 
-      comfirmQuery($create_post_query);
-
-
+        comfirmQuery($edit_user_query);
 }
 ?>
 
@@ -34,21 +53,34 @@ if(isset($_POST['edit_user'])){
 
  <div class="form-group">
          <label for="title">Firstname</label>
-          <input type="text" class="form-control" name="user_firstname">
+          <input type="text" class="form-control"  value="<?php echo $user_firstname;?>" name="user_firstname">
       </div>
 
          <div class="form-group">
          <label for="title">Lastname</label>
-          <input type="text" class="form-control" name="user_lastname">
+          <input type="text" class="form-control" value="<?php echo $user_lastname;?>" name="user_lastname">
       </div>
 
 
 
         <div class="form-group">
          <select name="user_role" id="">
-           <option value="subscriber">Select Options</option>
-            <option value="admin">Admin</option>
-            <option value="subscriber">Subscriber</option>
+          <option value="subscriber"> <?php echo $user_role; ?></option>
+
+           <?php
+             if($user_role =='admin'){
+                 echo "<option value='subscriber'>subscriber</option>";
+             } else {
+
+                 echo "<option value='admin'>admin</option>";
+             }
+
+             ?>
+
+
+
+
+
 
          </select>
       </div>
@@ -61,23 +93,23 @@ if(isset($_POST['edit_user'])){
 
        <div  class="form-group">
          <label for="post_tags">Username</label>
-          <input type="text" class="form-control" name="username">
+          <input type="text" class="form-control" value="<?php echo $username;?>" name="username">
       </div>
 
  <div class="form-group">
          <label for="post_tags">Email</label>
-          <input type="email" class="form-control" name="user_email">
+          <input type="email" class="form-control" value="<?php echo $user_email;?>" name="user_email">
       </div>
 
  <div class="form-group">
          <label for="post_tags">Password</label>
-          <input type="password" class="form-control" name="user_password">
+          <input type="password" class="form-control" value="<?php echo $user_password;?>" name="user_password">
       </div>
 
 
 
        <div class="form-group">
-          <input class="btn btn-primary" type="submit" name="edit_user" value="Add User">
+          <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
       </div>
 
 
